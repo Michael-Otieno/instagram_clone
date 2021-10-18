a929ac79b7bdc3e2fb703cdb6383d73a581794dd
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-19 00:37:51 +0300

    add timeline view function

--- ig/views.py
+++ ig/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Post,Profile,Following,Comment
 from .forms import DetailsForm,PostForm
+from django.db.models import F
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
@@ -36,3 +37,31 @@ def profile(request):
         form1 = PostForm()
     
     return render(request, 'profile.html', {"form":form,"form1":form1,"posts":posts,"followingcount":followingcount,"followercount":followercount})
+
+@login_required(login_url='/accounts/login/')
+def timeline(request):
+    users = User.objects.all()
+    posts = Post.objects.all()
+    follows = Following.objects.all()
+    comments = Comment.objects.all()
+    if request.method=='POST' and 'follow' in request.POST:
+        following=Following(username=request.POST.get("follow"),followed=request.user.username)
+        following.save()
+        return redirect('timeline')
+    elif request.method=='POST' and 'comment' in request.POST:
+        comment=Comment(comment=request.POST.get("comment"),
+                        post=int(request.POST.get("posted")),
+                        username=request.POST.get("user"),
+                        count=0)
+        comment.save()
+        comment.count=F('count')+1
+        return redirect('timeline')
+    elif request.method=='POST' and 'post' in request.POST:
+        posted=request.POST.get("post")
+        for post in posts:
+            if (int(post.id)==int(posted)):
+                post.like+=1
+                post.save()
+        return redirect('timeline')
+    else:
+        return render(request, 'timeline.html',{"users":users,"follows":follows,"posts":posts,"comments":comments})
