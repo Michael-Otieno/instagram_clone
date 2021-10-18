c1577b1b316b48e0b21aeffabfa6c86461bb2238
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-18 23:51:58 +0300

    add initial migration

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ig',
-    'bootstrap3',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'moringa',
+        'PASSWORD':'1234',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
