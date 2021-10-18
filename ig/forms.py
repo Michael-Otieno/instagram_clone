aba73d0f8ba8c35d659f57f2d00231c94a4bb38e
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-19 00:31:43 +0300

    add postform

--- ig/forms.py
+++ ig/forms.py
@@ -5,3 +5,8 @@ class DetailsForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['profile','date','like']
