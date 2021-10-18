7d68beda20bea056a8941b58d93b70bddcfc74e7
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-18 23:57:12 +0300

    add post class

--- ig/models.py
+++ ig/models.py
@@ -9,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+
+class Post(models.Model):
+    pic = models.ImageField(upload_to = 'posts/')
+    caption = models.CharField(blank=True,max_length = 255)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.profile.user.username}'
