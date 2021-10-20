ee1a47636454953799a76d02a8ab1bd6f6f1baeb
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-20 13:44:24 +0300

    add styles

--- ig/urls.py
+++ ig/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import path
 
 urlpatterns=[
-    url('', views.home,name="home"),
-    url('user/<str:username>', views.user_profile,name="user_profile"),
-    url('add_comment', views.add_comment,name="add_comment"),
-    url('like/<int:postid>',views.like_post,name='like_post'),
-    url('create/post', views.add_post, name='add_post'),
-    url('edit/<str:username>', views.edit_profile, name='edit_profile'),
+    path('', views.home,name="home"),
+    path('user/<str:username>', views.user_profile,name="user_profile"),
+    path('add_comment', views.add_comment,name="add_comment"),
+    path('like/<int:postid>',views.like_post,name='like_post'),
+    path('create/post', views.add_post, name='add_post'),
+    path('edit/<str:username>', views.edit_profile, name='edit_profile'),
 ]
 
 # if settings.DEBUG:
