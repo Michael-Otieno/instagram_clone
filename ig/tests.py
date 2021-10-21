2ffbd109289418da3b39de1c5fe7f3d04791a7ae
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-21 06:35:06 +0300

    add profile setup testclass

--- ig/tests.py
+++ ig/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post,Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(id=1,profile_pic='path/to/photo',user = self.user,biography='test bio')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
